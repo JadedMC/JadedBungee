b0724b47b26e243d6112996be014f9ea0ffce84a
colby <colbykrawczyk10@gmail.com>
2022-10-04 15:26:00 -0400

    Added some more utilities

--- src/main/java/net/jadedmc/jadedbungee/utils/ChatUtils.java
+++ src/main/java/net/jadedmc/jadedbungee/utils/ChatUtils.java
@@ -1,12 +1,56 @@
 package net.jadedmc.jadedbungee.utils;
 
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 /**
  * Some methods to make sending chat messages easier.
  */
 public class ChatUtils {
+    private final static int CENTER_PX = 154;
+
+    /**
+     * Sender a centered chat message to a CommandSender.
+     * @param sender Command Sender
+     * @param message Message
+     */
+    public static void centeredChat(CommandSender sender, String message) {
+        message = translate(message);
+
+        if(message == null || message.equals("")) sender.sendMessage("");
+        message = net.md_5.bungee.api.ChatColor.translateAlternateColorCodes('&', message);
+
+        int messagePxSize = 0;
+        boolean previousCode = false;
+        boolean isBold = false;
+
+        for(char c : message.toCharArray()) {
+            if(c == '§') {
+                previousCode = true;
+            }
+            else if(previousCode) {
+                previousCode = false;
+                isBold = c == 'l' || c == 'L';
+            }
+            else {
+                DefaultFontInfo dFI = DefaultFontInfo.getDefaultFontInfo(c);
+                messagePxSize += isBold ? dFI.getBoldLength() : dFI.getLength();
+                messagePxSize++;
+            }
+        }
+
+        int halvedMessageSize = messagePxSize / 2;
+        int toCompensate = CENTER_PX - halvedMessageSize;
+        int spaceLength = DefaultFontInfo.SPACE.getLength() + 1;
+        int compensated = 0;
+        StringBuilder sb = new StringBuilder();
+        while(compensated < toCompensate) {
+            sb.append(" ");
+            compensated += spaceLength;
+        }
+        sender.sendMessage(sb.toString() + message);
+    }
 
     /**
      * A quick way to send a player a colored message.
