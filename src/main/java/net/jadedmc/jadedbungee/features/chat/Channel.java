79f8a3c883915b6cc8c60d57329a7db2f2d081c0
colby <colbykrawczyk10@gmail.com>
2022-10-28 17:54:56 -0400

    Fix chat not being passed through

--- src/main/java/net/jadedmc/jadedbungee/features/chat/Channel.java
+++ src/main/java/net/jadedmc/jadedbungee/features/chat/Channel.java
@@ -80,12 +80,12 @@ public abstract class Channel {
     public boolean chat(ProxiedPlayer player, String message) {
         // Exits if the message should be handled by the sub server.
         if(passToSubServer) {
-            return true;
+            return false;
         }
 
         // Processes the chat filter.
         if(!passesFilter(player, message)) {
-            return false;
+            return true;
         }
 
         // Displays the chat message to each viewer.
@@ -95,7 +95,7 @@ public abstract class Channel {
 
         // Logs the chat message.
         plugin.channelManager().log(this.getName(), player, message);
-        return passToSubServer;
+        return !passToSubServer;
     }
 
     /**
